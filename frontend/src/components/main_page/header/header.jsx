46e68712e777b4df3117ed766c64f835418a21e4
Andre <andre.lls@hotmail.com>
2020-11-20 18:58:22 -0500

    Start header component

--- frontend/src/components/main_page/header/header.jsx
+++ frontend/src/components/main_page/header/header.jsx
@@ -1,8 +1,16 @@
+import React from 'react';
 
 export default function PageHeader() {
     return(
         <section className="header">
-            
+            <div className="right">
+                <h1>Clube Universo</h1>
+            </div>
+            <nav className='left'>
+                <ul>
+                    <li>Pathfinder Online</li>
+                </ul>
+            </nav>
         </section>
     )
 }
--- frontend/src/components/main_page/main.jsx
+++ frontend/src/components/main_page/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../main_page/header/header';
 
 export default function Main() {
     return(
         <div className="main">
-            <h1>Clube Universo!</h1>
+            <Header />
         </div>
     )
 }
