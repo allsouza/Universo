3e98c07e7ee06035f41eaad0dc00820fc3884ca9
Andre <andre.lls@hotmail.com>
2020-12-01 20:57:50 -0500

    Add photo carousel to main page

--- frontend/src/components/main_page/main.jsx
+++ frontend/src/components/main_page/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../main_page/header/header';
+import Photos from './photos';
 
 export default function Main() {
     return(
         <div className="main">
             <Header />
+            <Photos />
         </div>
     )
 }
---
+++ frontend/src/components/main_page/photos.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+export default function Photos() {
+    const PHOTOS = [
+        "https://clubeuniverso.s3.us-east-2.amazonaws.com/Photos/IMG-20200819-WA0000.jpg",
+        "https://clubeuniverso.s3.us-east-2.amazonaws.com/Photos/IMG-20200819-WA0001.jpg",
+        "https://clubeuniverso.s3.us-east-2.amazonaws.com/Photos/IMG-20200819-WA0010.jpg",
+        "https://clubeuniverso.s3.us-east-2.amazonaws.com/Photos/IMG-20200819-WA0014.jpg",
+        "https://clubeuniverso.s3.us-east-2.amazonaws.com/Photos/IMG-20200819-WA0015.jpg",
+        "https://clubeuniverso.s3.us-east-2.amazonaws.com/Photos/IMG-20200819-WA0017.jpg"
+    ]
+
+    return(
+        <Carousel className='photos'
+            infiniteLoop={true}
+            transitionTime={2000}
+            dynamicHeight={true}
+            interval={7000}
+            swipeable={true}
+            autoPlay={true}>
+            {PHOTOS.map(photo => {
+                return(
+                    <div key={photo.slice(photo.length-10)} className='picture'>
+                        <img src={photo} alt=""/>
+                    </div>
+                )
+            })}
+        </Carousel>
+    )
+}
