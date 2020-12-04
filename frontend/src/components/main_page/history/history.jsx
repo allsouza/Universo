412cfe3154c955fe2a80e05741f2641a7bc31c8c
Andre <andre.lls@hotmail.com>
2020-12-04 15:09:28 -0500

    Update history

--- frontend/src/components/main_page/history/history.jsx
+++ frontend/src/components/main_page/history/history.jsx
@@ -4,12 +4,12 @@ import { connect } from 'react-redux';
 function History({language}) {
     return(
         <div className="history">
-            {text.body[language]}
+            {txt.body[language]}
         </div>
     )
 }
 
-const text = {
+const txt = {
     body: [
         <section>
             <h1>Pathfinder History</h1>
@@ -30,7 +30,7 @@ const text = {
             <p>
                 By 1947-48 Southern California Conference began having Pathfinder clubs - the first at Glendale, with Lawrence Paulson as director.  About that same time, the Central California Conference, under the direction of Youth Director Henry T. Bergh, began their Pathfinder program -- starting 23 clubs that first year.
             </p>
-            <h2>Officialization</h2>
+            <h2>Becoming official</h2>
             <p>
                 Beginning with the God-directed program, called Pathfinder Clubs, in California, the General Conference of the Seventh-day Adventist church adopted the program.  It thus, in 1950, became an official worldwide organization of the Adventist church, and grew rapidly.
             </p>
@@ -43,21 +43,21 @@ const text = {
             <p>
                 O nome Desbravadores foi usado por Theron Johnston, em 1930, quando organizou um clube em sua casa na cidade Santa Ana, Califórnia, nos Estados Unidos. Não recebeu apoio e acabou abandonando a idéia.
             </p>
+            <h2>Desenvolvimento</h2> 
             <p>
                 Já em 1940, a Associação do Sudoeste da Califórnia chamou o seu acampamento de "Acampamento de Desbravadores Jovens Missionários Voluntários". Nessa época, também surgiu um clube organizado pelo pastor Laurence Skinner com o nome "Locomotiva".
             </p>
-            <h2>O Desenvolvimento</h2> 
             <p>
                 O clube começou a tomar corpo a partir de 1946, com a liderança do pastor John Hancock, que era diretor de jovens da Associação do Sudoeste da Califórnia. Aproveitando o nome do acampamento da Associação, chamaram de "Clube dos Desbravadores Jovens Missionários Voluntários".
             </p>
-            <h2>A Organização</h2>
+            <h2>Organização</h2>
             <p>
                 Em 1946, o próprio pastor Hancock desenhou o emblema em forma de triângulo, que ainda é usado em todo o mundo.
             </p>
             <p>
                 Em 1947, a Associação Geral pediu à União do Pacífico para desenvolver a Organização do Clube de Desbravadores. O pastor J. R. Nelson coordenou este trabalho. Em seguida, Lawrence Paulson escreveu os primeiros manuais de orientação. Em maio de 1949, o pastor Henry Berg, mesmo não sendo músico, compôs o Hino dos Desbravadores.
             </p>
-            <h2>A Oficialização</h2>
+            <h2>Oficialização</h2>
             <p>
                 Em 1950 o departamento de Jovens da Associação Geral adotou oficialmente o "Clube de Desbravadores Jovens Missionários Voluntários" como um programa mundial. Em 1952 o Hino dos Desbravadores foi oficializado e passou a fazer parte do programa.
             </p>
